c8609c8b81704940a5d7bd718e33a562fe9cfa0c
Sam29W <samithshivakumar1@gmail.com>
2025-11-22 22:24:34 +0530

    YOLO image prediction model
    Add your image source and try it out

--- Yolo Model.py
+++ Yolo Model.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 model = YOLO("yolo11n.pt")
-results = model.predict(source="https://hips.hearstapps.com/ghk.h-cdn.co/assets/17/30/dachshund.jpg")
+results = model.predict(source="")
 
 for r in results:
     r.show()
